3ea5bfddb11e2e6e71ab481809d341611aea89d8
Sotheanith Sok <thetheanith@gmail.com>
2017-01-24 12:14:20 -0800

    Completed Lab1

---
+++ Lab1-Multiplexer/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/Workspace/CECS341-ProjectCollection/Lab1-Multiplexer/muxvtf_isim_beh.exe" -prj "D:/Workspace/CECS341-ProjectCollection/Lab1-Multiplexer/muxvtf_beh.prj" "work.muxvtf" "work.glbl" 
